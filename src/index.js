700fd9db913fc210a71efa38d4e10664efdfd066
Lucy Copp <lucyanncopp@hotmail.co.uk>
2019-05-24 13:55:43 +0100

    Added routing

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 
 
+
 ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById("root")
-  );
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
   
 
 serviceWorker.unregister();
