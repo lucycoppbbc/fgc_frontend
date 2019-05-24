0705a378b6915304afb8a6674b8d91e95f2223ae
Lucy Copp <lucyanncopp@hotmail.co.uk>
2019-05-24 22:21:10 +0100

    added front end for login page

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,14 +1,65 @@
 import React, { Component } from "react";
 import { Auth } from "aws-amplify";
-import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
-import "./Login.css";
+import "./Login.scss";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit() {}
+  renderForm() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+      <FormGroup controlId="email" bsSize="large">
+        <ControlLabel className="log-in__text">Email</ControlLabel>
+        <FormControl
+          className="log-in__input-text"
+          type="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+      </FormGroup>
+      <FormGroup controlId="password" bsSize="large">
+        <ControlLabel className="log-in__text">Password</ControlLabel>
+        <FormControl
+          value={this.state.password}
+          type="password"
+          className="log-in__input-text"
+          onChange={this.handleChange}
+        />
+      </FormGroup>
+      <div className="log-in__button_wrapper">
+      <button 
+       className="log-in__button"
+        // disabled={!this.validateForm()}
+        type="submit"
+      >
+        Login
+      </button>
+      </div>
+    </form>
+    )
+  }
+
+
   render() {
     return (
       <div className="Login">
-       <p>LOGIN</p>
+       {this.renderForm()}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Amplify from "aws-amplify";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import config from "./config";
+import "./index.scss";
 
-
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "products",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 ReactDOM.render(
   <Router>
@@ -13,6 +38,5 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-  
 
 serviceWorker.unregister();
